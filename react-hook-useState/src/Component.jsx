0fa5a52ecc88ff1d5b8806eedaf6cc510c99b876
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-22 21:00:58 +0530

    boolean setter using useState() react hook

--- react-hook-useState/src/Component.jsx
+++ react-hook-useState/src/Component.jsx
@@ -3,11 +3,18 @@ import React, {useState} from 'react';
 function Component() {
     const [name, setName] = useState("guest");
     const [age, setAge] = useState(0);
+    const [isEmployed, setisEmployed] = useState(false);
+    
     const updateName = () => {
         setName("ninja");
     }
     const incrementAge = () => {
         setAge(age + 1);
+    }
+    const toggleIsEmployed = () => {
+        /* inorder to toggle between true and false
+         we use ! (not operator) */
+        setisEmployed(!isEmployed);
     }
     return(
         <div>
@@ -15,7 +22,10 @@ function Component() {
             <button onClick={updateName} > set name </button>
 
             <p> Age: {age} </p>
-            <button onClick={incrementAge} > incriment age </button>
+            <button onClick={incrementAge} > increment age </button>
+
+            <p> is Employed: {isEmployed? "yes":"no"} </p>
+            <button onClick={toggleIsEmployed} > set Employed status </button>
         
         </div>
     );
